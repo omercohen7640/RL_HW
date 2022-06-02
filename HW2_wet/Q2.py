4ea6ceba3708909d1c67f6c2506e84b283a1c49b
dvirmarsh <50172260+dvirmarsh@users.noreply.github.com>
2022-06-02 18:07:28 +0300

    Q2 started

--- HW2_wet/Q2.py
+++ HW2_wet/Q2.py
@@ -54,6 +54,43 @@ def pi_c_policy():
         pi_c[i] = jobs[idx]
     return pi_c
 
+def c_mu_policy():
+    pi = np.zeros(32, dtype=int)
+    for i in range(31):
+        jobs = list(index_dict[i])
+        idx = np.argmax([c[j]*mu[j] for j in jobs])
+        pi[i] = jobs[idx]
+    return pi
+
+
+def policy_iteration():
+    pi = pi_c_policy()
+    V = solve_belman(pi)
+    V_s0 = [V[0].copy()]
+    rewards = np.zeros((32, 1))
+    for i in range(31):
+        state = list(index_dict[i])
+        for job in state:
+            rewards[i] -= c[job]
+    for j in range(20):
+        for i in reversed(range(31)):
+            state = list(index_dict[i])
+            for job in state:
+                ref_state = state.copy()
+                ref_state.remove(job)
+                next_idx = state_dict[tuple(ref_state)]
+                val = rewards[i] + (1-mu[job])*V[i] +mu[job]*V[next_idx]
+                if val > V[i]:
+                    V[i] = val
+                    pi[i] = job
+        V_s0.append(V[0].copy())
+
+    return pi, V_s0
+
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -66,3 +103,18 @@ if __name__ == '__main__':
     plt.title("$Value$ $Function$ $V^\pi$ $of$ $Max$ $Policy$ $\pi_c$")
     plt.show()
 
+
+    pi_star, V_s0 = policy_iteration()
+
+    plt.plot(range(len(V_s0)), V_s0, color='lightskyblue')
+    plt.ylabel('$Value$ $Function$')
+    plt.xlabel('$iteration$')
+    plt.title("$Initial$ $State$ $S_0$ $Value$ $Function$ $for$ $Policy$ $Iteration$")
+    plt.show()
+
+    pi_c_mu = c_mu_policy()
+    V_c_mu = solve_belman(pi)
+
+
+
+    a=0
