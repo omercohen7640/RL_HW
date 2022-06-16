b393f4626f3ebe27c03ed39dddde599ef9935120
dvirmarsh <50172260+dvirmarsh@users.noreply.github.com>
2022-06-16 13:53:31 +0300

    started

--- HW3_wet/main.py
+++ HW3_wet/main.py
@@ -5,6 +5,19 @@ import numpy as np
 from data_collector import DataCollector
 from data_transformer import DataTransformer
 from lspi import compute_lspi_iteration
+from game_player import GamePlayer
+from linear_policy import LinearPolicy
+
+def evaluation(env, data_transformer, feature_extractor, policy):
+    max_steps_per_game = 1000
+    number_of_games = 50
+    play = GamePlayer(env, data_transformer, feature_extractor, policy)
+    start_pos = np.random.uniform(env.low[0], env.high[0], size=number_of_games)
+    all_results = [play.play_game(max_steps_per_game, start_state=[start_pos[i], 0]) for i in range(number_of_games)]
+    success_rate = np.mean(all_results)
+    #print(f'success rate is {success_rate}')
+    return success_rate
+
 
 if __name__ == '__main__':
 
@@ -47,9 +60,16 @@ if __name__ == '__main__':
 
     # Question 3 section 5
 
+
     w_updates = 20
+    gamma = 0.999
+    data_transformer = DataTransformer()
     for seed in [110, 123, 136]:
         np.random.seed(seed)
+        linear_policy = LinearPolicy(120, 3, include_bias=False)
+        states, actions, rewards, next_states, done_flags = DataCollector(env).collect_data(samples_to_collect)
+        encoded_states = feature_extractor.encode_states_with_radial_basis_functions(states)
+        encoded_next_states = feature_extractor.encode_states_with_radial_basis_functions(next_states)
         for lspi_iteration in range(w_updates):
             print(f'starting lspi iteration {lspi_iteration}')
 
@@ -59,3 +79,4 @@ if __name__ == '__main__':
             norm_diff = linear_policy.set_w(new_w)
             if norm_diff < 0.00001:
                 break
+        success_rate = evaluation(env, data_transformer, feature_extractor, linear_policy)
